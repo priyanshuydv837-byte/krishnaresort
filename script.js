1843f735e5b1aecbacd3b70bac24a04fac8a9f4d
priyanshuydv837-byte <priyanshuydv837@gmail.com>
2026-04-01 13:48:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let submitBtn = document.querySelector(".formSubmitBtn");
+
+submitBtn.addEventListener("click",(e)=>{
+  Swal.fire({
+  title: "Booking Succesfully!",
+  text: "Have A Great Day",
+  icon: "success"
+});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let userName = document.querySelector(".bookingUserName");
+// let userEmail = document.querySelector(".bookingUserEmail");
+// let bookingDate = document.querySelector(".bookingUserDate");
+
+// // console.dir(userName);
+
+// console.log("Hello:");
